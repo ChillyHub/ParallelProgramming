c9f2692c9c9a288f4c984bab62aa216a1dd2a189
Ben Chilly <1533935729@qq.com>
2023-11-03 01:17:46 +0800

    commit

--- MPI1Proc8.cpp
+++ MPI1Proc8.cpp
@@ -2,6 +2,8 @@
 
 #include "mpi.h"
 
+#include <vector>
+
 void Solve()
 {
     Task("MPI1Proc8");
@@ -13,4 +15,24 @@ void Solve()
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
+    int N;
+    pt >> N;
+
+    double sum = 0;
+    for (int i = 0; i < N; i++)
+    {
+        double num;
+        pt >> num;
+
+        sum += num;
+    }
+
+    if (rank % 2)
+    {
+        pt << sum / N;
+    }
+    else
+    {
+        pt << sum;
+    }
 }
